598953c475dcbc88dc5d29841e09558a696e0ae0
Raunak0713 <raunakbhanarkar7@gmail.com>
2025-04-09 16:49:59 +0530

    Layout Change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,3 @@
-import type { Config } from "tailwindcss";
-
 export default {
 	darkMode: ["class"],
 	content: [
@@ -131,4 +129,4 @@ export default {
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
